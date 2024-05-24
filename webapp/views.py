fd34728f48fd3003bae6c2a4167f2d2a8be76ffb
Sebastian Rojas Gomez <88469107+Petulio14@users.noreply.github.com>
2024-05-23 22:26:51 -0500

    update forms

--- webapp/views.py
+++ webapp/views.py
@@ -32,7 +32,7 @@ def reservar(request):
                 pago = False
             
             # Guardar la reserva
-            reserva = Reservas(nombre=nombre, apellido=apellido, cancha=cancha, fecha=fecha_hora, hora=hora, duracion=duracion, cel=cel, pago=pago)
+            reserva = Reservas(nombre=nombre, apellido=apellido, cancha=cancha, fecha=fecha, hora=hora, duracion=1, cel=cel, pago=pago)
             reserva.save()
             return render(request, 'reservar.html', {'mensaje': 'Se generó una reserva a nombre de: ' + nombre})
     return render(request, "reservar.html")
